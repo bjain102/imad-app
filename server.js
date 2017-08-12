68d8342a4c4bf7617678f9098764f55794172fac
bjain102 <30688626+bjain102@users.noreply.github.com>
2017-08-12 13:07:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,21 +6,21 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles = {
-    var articleOne: {
+    'article-one': {
       title: 'Article One|Bharath Jain',
       heading: 'Article One',
       content: `<p>
                 This is my first article.
                 </p>`
     },
-    var articleTwo: {
+    'article-two': {
               title: 'Article Second|Bharath Jain',
       heading: 'Article Second',
       content: `<p>
                 This is my second article.
                 </p>`
     },
-    var articleThree: {
+    'article-three': {
               title: 'Article Three|Bharath Jain',
       heading: 'Article Three',
       content: `<p>
@@ -69,16 +69,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send(createTemplate(articleOne));
-});
-
-app.get('/article-two', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName', function (req, res) {
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
